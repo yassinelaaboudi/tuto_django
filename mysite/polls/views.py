c60bb795240f6f48d6306f80759075c7b6f1c87e
yassinelaaboudi <yassine_laaboudi@hotmail.com>
2021-05-18 20:34:59 +0200

    next part

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question 
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello you are at the poll index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request,"polls/index.html",context)
+
+
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})
+
+def results(request,question_id):
+    response = "you're looking at the result of question {}"
+    return HttpResponse(response.format(question_id))
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/details.html',
+        {'question':question,"error_message":"You didn't select a choice"}
+        )
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponse(reverse('polls:results',args=('question.id,')))
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/results.html",{"question":question})
+
+if __name__ == "__main__":
+    loader.get_template('polls/index.html')
